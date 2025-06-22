b509dfe2273d7a160f6daad116413204be6ccb47
RichieQuintana <103153905+RichieQuintana@users.noreply.github.com>
2025-06-22 07:31:19 +0000

    feat: usar clase Despedida en Main

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
+    }
+}
